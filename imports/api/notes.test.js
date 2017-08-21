bc48044276a4d2611cf294a3f578295a428f6664
disc0ninja <disc0ninja77@gmail.com>
2017-08-21 10:24:55 -0500

    Testing: Notes Publications: Added test coverage to ensure users are only able to access notes they created with publications.

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,6 +5,7 @@ import { Notes } from './notes';
 
 if (Meteor.isServer) {
   describe('notes', function () {
+
     const noteOne = {
       _id: 'testNoteId1',
       title: 'My Title',
@@ -12,6 +13,15 @@ if (Meteor.isServer) {
       updatedAt: 0,
       userId: 'testUserId1'
     }
+
+    const noteTwo = {
+      _id: 'testNoteId2',
+      title: 'Things to buy',
+      body: 'couch',
+      updatedAt: 0,
+      userId: 'testUserId2'
+    }
+
     beforeEach( function () {
       Notes.remove({});
       Notes.insert(noteOne);
@@ -93,6 +103,23 @@ if (Meteor.isServer) {
       expect(() => {
         Meteor.server.method_handlers['notes.update'].apply({userId: noteOne.userId}, []);
       }).toThrow();
+    })
+
+    it('should return a users notes', function () {
+      const res = Meteor.server.publish_handlers.notes.apply({userId: noteOne.userId})
+
+      const notes = res.fetch();
+
+      expect(notes.length).toBe(1);
+      expect(notes[0]).toEqual(noteOne);
+    })
+
+    it('should return 0 notes for a user with no notes', function () {
+      const res = Meteor.server.publish_handlers.notes.apply({userId: 'testId'})
+
+      const notes = res.fetch();
+
+      expect(notes.length).toBe(0);
     })
 
   });
