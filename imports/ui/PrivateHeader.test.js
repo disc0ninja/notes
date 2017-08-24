0b6813dfa4e71a79aed157bd62cf6be0a557d61c
disc0ninja <disc0ninja77@gmail.com>
2017-08-23 20:34:30 -0500

    Testing: React: PrivateHeader: Added two test cases to ensure that the component is rendering its props correctly, and buttons

---
+++ imports/ui/PrivateHeader.test.js
@@ -0,0 +1,29 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import expect from 'expect';
+import { mount } from 'enzyme';
+
+import PrivateHeader from './PrivateHeader';
+
+if (Meteor.isClient) {
+  describe('PrivateHeader', function () {
+    it('should set button text to logout', function () {
+      const wrapper = mount( <PrivateHeader title="Test title"/> )
+
+      const buttonText = wrapper.find('button').text();
+
+      expect(buttonText).toBe('Logout');
+    });
+
+    it('should use title prop as h1 text', function () {
+      const title = 'Test title';
+      const wrapper = mount( <PrivateHeader title={title} /> );
+
+      const h1Text = wrapper.find('h1').text();
+
+      expect(h1Text).toBe(title);
+
+    });
+
+  });
+}
