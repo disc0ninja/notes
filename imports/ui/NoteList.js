cab8b60a4773181350aaf1b8cf588715cf517123
disc0ninja <disc0ninja77@gmail.com>
2017-08-26 16:58:18 -0500

    Feature: NotesList: NotesListEmptyItem: Created a component, NotesListEmptyItem, that is conditionally rendered when the users notes list is empty. IT displays a notice to the user that they currently don't have any notes, and recommonds adding a note to get started

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -2,21 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Meteor} from 'meteor/meteor';
 import {createContainer} from 'meteor/react-meteor-data';
-
+// api imports
+import {Notes} from '../api/notes';
 // component imports
 import NoteListHeader from './NoteListHeader';
 import NoteListItem from './NoteListItem';
-
-// api imports
-import {Notes} from '../api/notes';
+import NoteListEmptyItem from './NoteListEmptyItem';
 
 export const NoteList = (props) => {
   return (
     <div>
       <NoteListHeader/>
       {props.notes.map((note) => {
-        return <NoteListItem key={note._id} note={note}/>;
+        return <NoteListItem key={note._id} note={note} />
       })}
+      {props.notes.length === 0 ?  <NoteListEmptyItem/> : undefined}
       NoteList {props.notes.length}
     </div>
   );
---
+++ imports/ui/NoteListEmptyItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoteListEmptyItem = () => {
+  return (
+    <div>
+      <h5>You dont't have any notes yet.</h5>
+      <p>Create a note to get started.</p>
+    </div>
+  )
+}
+
+export default NoteListEmptyItem;
