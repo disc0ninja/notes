3d4326017b8773c343eade0221be0635ac35e7b1
disc0ninja <disc0ninja77@gmail.com>
2017-08-26 09:59:44 -0500

    Feature: Login: Updated the login component using a container to render, and pass the loginWithPassword functionality, to enable better testability of the Login component

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router';
-import { Meteor } from 'meteor/meteor';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router';
+import {Meteor} from 'meteor/meteor';
+import {createContainer} from 'meteor/react-meteor-data';
 
-export default class Login extends React.Component {
+export class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -14,7 +16,9 @@ export default class Login extends React.Component {
     let email = this.refs.email.value.trim();
     let password = this.refs.password.value.trim();
 
-    Meteor.loginWithPassword({email}, password, (err) => {
+    this.props.loginWithPassword({
+      email
+    }, password, (err) => {
       if (err && err.reason == 'Match failed') {
         this.setState({error: 'Login failed. Please check username and password'});
       } else if (err) {
@@ -29,11 +33,13 @@ export default class Login extends React.Component {
       <div className="boxed-view">
         <div className="boxed-view__box">
           <h1>Login</h1>
-          {this.state.error ? <p>{this.state.error}</p> : undefined }
+          {this.state.error
+            ? <p>{this.state.error}</p>
+            : undefined}
           <form className="boxed-view__form" onSubmit={this.onSubmit.bind(this)} noValidate>
             <input type="email" ref="email" name="email" placeholder="Email"/>
-            <input type="password" ref="password" name="password" placeholder="Password" />
-            <button className="button" >Login</button>
+            <input type="password" ref="password" name="password" placeholder="Password"/>
+            <button className="button">Login</button>
           </form>
           <Link to="/signup">Need an account?</Link>
         </div>
@@ -41,3 +47,11 @@ export default class Login extends React.Component {
     );
   };
 };
+
+Login.propTypes = {
+  loginWithPassword: PropTypes.func.isRequired
+}
+
+export default createContainer(() => {
+  return {loginWithPassword: Meteor.loginWithPassword};
+}, Login);
