da7cc3c52b0ad6eab854d160a30ed40f1e64df25
disc0ninja <disc0ninja77@gmail.com>
2017-08-26 17:53:58 -0500

    Testing: React: NoteListItem: refactored NoteListItem to use fixture data for testing. Added test scenario to ensure Session(selectedNoteId) is set when a user clicks on the note.

--- imports/ui/NoteListItem.test.js
+++ imports/ui/NoteListItem.test.js
@@ -3,23 +3,38 @@ import expect from 'expect';
 import { Meteor } from 'meteor/meteor';
 import { mount } from 'enzyme';
 
-import NoteListItem from './NoteListItem';
+import {NoteListItem} from './NoteListItem';
+import {notes} from '../fixtures/fixtures';
 
 if (Meteor.isClient) {
   describe('NoteListItem', function () {
-    it('should render title and timestamp', function () {
-      const title = 'Test Title';
-      const updatedAt = 1503780230094;
-      const wrapper = mount( <NoteListItem note={{title, updatedAt}} />);
+    let Session;
 
-      expect(wrapper.find('h5').text()).toBe(title);
-      expect(wrapper.find('p').text()).toBe('8/26/17');
+    beforeEach(() => {
+      Session={
+        set: expect.createSpy()
+      };
+    });
+
+    it('should render title and timestamp', function () {
+      const wrapper = mount( <NoteListItem note={notes[0]} Session={Session}/>);
+
+      expect(wrapper.find('h5').text()).toBe(notes[0].title);
+      expect(wrapper.find('p').text()).toBe('12/31/69');
     });
 
     it('should set default title if no title set', function () {
-      const wrapper = mount(<NoteListItem note={{}} />);
+      const wrapper = mount(<NoteListItem note={notes[1].title} Session={Session}/>);
       expect(wrapper.find('h5').text()).toBe('Untitled note');
     });
+
+    it('should call set on click', function () {
+      const wrapper = mount( <NoteListItem note={notes[0]} Session={Session}/>);
+
+      wrapper.find('div').simulate('click');
+
+      expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id);
+    })
 
   });
 };
