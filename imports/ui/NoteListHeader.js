7e2df33629e5d33aeadbb82d2daf1bc64ffffc95
disc0ninja <disc0ninja77@gmail.com>
2017-08-30 10:34:37 -0500

    Feature: create Notes: Added functionality to select newly created notes, upon creation for quick and easy editing

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -2,19 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Meteor} from 'meteor/meteor';
 import {createContainer} from 'meteor/react-meteor-data';
+import {Session} from 'meteor/session';
 
 import {Notes} from '../api/notes';
 
 export const NoteListHeader = (props) => {
   return (
-    <button onClick={() => props.meteorCall('notes.insert')}>Create Note</button>
+    <button onClick={() => props.meteorCall('notes.insert', (err, res) => {
+      if (res) {
+        props.Session.set('selectedNoteId', res);
+      }
+    })}>Create Note</button>
   );
 };
 
 NoteListHeader.propTypes = {
-  meteorCall: PropTypes.func.isRequired
+  meteorCall: PropTypes.func.isRequired,
+  Session: PropTypes.object.isRequired
 }
 
 export default createContainer(() => {
-  return {meteorCall: Meteor.call};
+  return {meteorCall: Meteor.call, Session};
 }, NoteListHeader);
